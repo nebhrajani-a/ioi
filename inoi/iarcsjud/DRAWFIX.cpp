c4856c80a4a98d693be45d175f4d7eaa723395a6
nebhrajani-a <aditya.v.nebhrajani@gmail.com>
2020-11-27 17:35:06 +0530

    Fixed filename.

---
+++ inoi/iarcsjud/DRAWFIX.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define INF 0x3f3f3f3f
+
+int main()
+{
+  std::vector<int> s = {1873, 2134, 1900, 1600};
+  std::vector<int> n = {2450, 1860, 1700, 2120};
+  return 0;
+}
